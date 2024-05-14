8cef5a99edbeba191c387321087097eb0cacec6b
StoneDwarf <131169770+StoneDwarf@users.noreply.github.com>
2024-05-14 15:50:52 +0400

    Update Flashcard.py

--- Flashcard.py
+++ Flashcard.py
@@ -1,132 +1,146 @@
 import json
+import io
+
+log_buffer = io.StringIO()
 
 
-def term_error(dat):
-    term = str(input())
+def log_input():
+    user_input = input()
+    log_buffer.write(user_input + '\n')
+    return user_input
+
+def log_print(message):
+    print(message)
+    log_buffer.write(message + '\n')
+    
+
+def term_error(database):
+    term = log_input()
     while True:
-        if term in dat:
-            print(f'The term "{term}" already exists. Try again:')
-            term = input()
+        if term in database.keys():
+            log_print(f'The term "{term}" already exists. Try again:')
+            term = log_input()
         else:
             return term
 
 
-def definition_error(dat):
-    definition = str(input())
+def definition_error(database):
+    definition = log_input()
     while True:
-        if definition in dat.values():
-            print(f'The definition "{definition}" already exists. Try again:')
-            definition = input()
+        if definition in database.values():
+            log_print(f'The definition "{definition}" already exists. Try again:')
+            definition = log_input()
         else:
             return definition
 
 
-def answer_error(dat, answer, test_reply):
-    if test_reply in dat.values():
-        for wrong_question, wrong_answer in dat.items():
+def answer_error(database, answer, test_reply):
+    if test_reply in database.values():
+        for wrong_question, wrong_answer in database.items():
             if test_reply == wrong_answer:
-                print(f'Wrong. The right answer is "{answer}", but your definition is correct for "{wrong_question}".')
+                log_print(f'Wrong. The right answer is "{answer}", but your definition is correct for "{wrong_question}".')
     else:
-        print(f'Wrong. The right answer is "{answer}".')
+        log_print(f'Wrong. The right answer is "{answer}".')
 
 
-def add_cart(dat):
-    print('The term for card:')
-    term = term_error(dat)
-    print('The definition for card ')
-    definition = definition_error(dat)
-    dat[term] = definition
-    print(f'The pair ("{term}":"{definition}") has been added')
+def add_cart(database):
+    log_print('The term for card:')
+    term = term_error(database)
+    log_print('The definition for card ')
+    definition = definition_error(database)
+    database[term] = definition
+    log_print(f'The pair ("{term}":"{definition}") has been added')
 
 
-def remove_cart(dat):
-    print('Which card?')
-    card = input()
+def remove_cart(database):
+    log_print('Which card?')
+    card = log_input()
     try:
-        del dat[card]
-        print('The card card has been removed')
+        del database[card]
+        log_print('The card card has been removed')
     except KeyError:
-        print(f'Can\'t remove "{card}": there is no such card.')
+        log_print(f'Can\'t remove "{card}": there is no such card.')
 
 
-def import_database(dat):
-    print('File name:')
-    filename = input()
+def import_database(database):
+    log_print('File name:')
+    filename = log_input()
     merged_dict = {}
     try:
         with open(filename, 'r') as new_data:
             downloaded_dict = json.load(new_data)
             merged_dict.update(downloaded_dict.get('database', {}))
             cards_number = len(merged_dict)
-            merged_dict.update(dat)
-            dat = merged_dict
+            merged_dict.update(database)
+            database = merged_dict
             error_database.update(downloaded_dict.get('error_database', {}))
-            print(f'{cards_number} cards have been loaded')
-            return dat
+            log_print(f'{cards_number} cards have been loaded')
+            return database
     except FileNotFoundError:
         print('File not found')
 
 
-def write_file(dat):
-    print('File name:')
-    filename = input()
-    combined_data = {"database": dat, "error_database": error_database}
+def write_file(database):
+    log_print('File name:')
+    filename = log_input()
+    combined_data = {"database": database, "error_database": error_database}
     with open(filename, 'w') as new_data:
         json.dump(combined_data, new_data)
-        print(f'{len(dat)} cards have been saved')
+        log_print(f'{len(database)} cards have been saved')
 
 
-def ask_question(dat, error_dat):
-    print('How many times to ask?')
-    cards_nbr = int(input())
-    for _ in range(cards_nbr):
-        for question, answer in dat.items():
-            if cards_nbr > 0:
-                print(f'Print the definition of "{question}":')
-                test_reply = str(input())
+def ask_question(database, error_database):
+    log_print('How many times to ask?')
+    cards_nbr = log_input()
+    for _ in range(int(cards_nbr)):
+        for question, answer in database.items():
+            if int(cards_nbr) > 0:
+                log_print(f'Print the definition of "{question}":')
+                test_reply = log_input()
                 if test_reply == answer:
-                    print("Correct!")
+                    log_print("Correct!")
                 else:
-                    error_track(error_dat, question)
-                    answer_error(dat, answer, test_reply)
+                    error_track(error_database, question)
+                    answer_error(database, answer, test_reply)
                 cards_nbr -= 1
 
 
-def error_track(error_dat, wrong_question):
-    if wrong_question in error_dat.keys():
-        error_dat[wrong_question] += 1
+def error_track(error_database, wrong_question):
+    if wrong_question in error_database.keys():
+        error_database[wrong_question] += 1
     else:
-        error_dat[wrong_question] = 1
+        error_database[wrong_question] = 1
 
 
-def hardest_card(error_dat):
-    if len(error_dat) == 0:
-        print('There are no cards with errors.')
+def hardest_card(error_database):
+    if len(error_database) == 0:
+        log_print('There are no cards with errors.')
         return
     else:
-        max_errors = max(error_dat.values())
-        hardest_cards = [term for term, errors in error_dat.items() if errors == max_errors]
+        max_errors = max(error_database.values())
+        hardest_cards = [term for term, errors in error_database.items() if errors == max_errors]
         if len(hardest_cards) == 1:
-            print(f'The hardest card is "{hardest_cards[0]}". You have {max_errors} errors answering it.')
+            log_print(f'The hardest card is "{hardest_cards[0]}". You have {max_errors} errors answering it.')
         else:
             hardest_cards_str = ', '.join([f'"{term}"' for term in hardest_cards])
-            print(f'The hardest cards are {hardest_cards_str}. You have {max_errors} errors answering them.')
+            log_print(f'The hardest cards are {hardest_cards_str}. You have {max_errors} errors answering them.')
 
 
 def make_log(log_data):
-    print('File name:')
-    filename = input()
-    with open(filename, 'w') as new_data:
-        json.dump(log_data, new_data)
-    print('The log has been saved.')
+    log_print('File name:')
+    filename = log_input()
+    with open(filename, 'w') as log_file:
+        log_file.write(log_data.getvalue())
+    log_print('The log has been saved.')
 
 
 database = {}
 error_database = {}
 
+
 while True:
-    print('Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):')
-    action = input()
+    log_print('Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):')
+    action = log_input()
     if action == 'add':
         add_cart(database)
     elif action == 'remove':
@@ -141,7 +155,7 @@ while True:
         print('Bye bye!')
         break
     elif action == 'log':
-        make_log(my_log)
+        make_log(log_buffer)
     elif action == 'hardest card':
         hardest_card(error_database)
     elif action == 'reset stats':
